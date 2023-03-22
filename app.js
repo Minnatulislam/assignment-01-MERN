8c18b04864692f75d677388af2192ea950435f6f
Minnatulislam <minnatulislam86@gmail.com>
2023-03-22 22:06:42 +0600

    multiplication table done

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-function multiplicationTable(num) {
-    for (let i = 1; i <= 10; i++) {
-      const product = num * i;
-      console.log(`${num} x ${i} = ${product}`);
+
+  function multiplicationTable(num){
+    for(let i = 1; i <= 10; i++){
+        const product = num * i;
+        console.log(`${num} * ${i} = ${product}`);
     }
   }
 
-  multiplicationTable(5);  /* call the function with different aregument(definitely int) */
+  multiplicationTable(4); /* call the function with different aregument(definitely int) */
